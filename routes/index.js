c3973ef629d944462921cc4771e9f466dd2d1c6e
briannkhata <briannkhata@gmail.com>
2023-03-20 21:12:02 +0200

    added more code to the routes

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const users = require('../controllers/user.js');
+const { encode } = require('../middlewares/jwt.js');
+
+const router = express.Router();
+
+router.post('/login/:userId', encode, (req, res, next) => { });
+
+export default router;
